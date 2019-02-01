f86be6a4f3b5fe8d15e751fc770a9ca2549fc3b6
zolandhole <zolandhole@gmail.com>
2019-02-01 13:30:00 +0700

    New Activity Launch - Home Activity

--- app/src/main/java/yandi/yarud/dialogislamgaruda/HomeActivity.java
+++ app/src/main/java/yandi/yarud/dialogislamgaruda/HomeActivity.java
@@ -2,12 +2,27 @@ package yandi.yarud.dialogislamgaruda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private ImageView imageViewUmrah, imageViewHaji;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        imageViewUmrah = findViewById(R.id.imageViewPanduanUmrah);
+        imageViewHaji = findViewById(R.id.imageViewPanduanHaji);
+
+        animaation();
+    }
+
+    private void animaation() {
+//        imageViewUmrah.setAlpha(0);
+        imageViewUmrah.setTranslationX(-1000);
+//        imageViewHaji.setAlpha(0);
+        imageViewHaji.setTranslationX(-1000);
+
+        imageViewUmrah.animate().translationX(10).setDuration(800).setStartDelay(500).translationX(0).start();
+        imageViewHaji.animate().translationX(10).setDuration(800).setStartDelay(700).translationX(0).start();
     }
 }
