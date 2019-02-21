d960ca8bbfdb701548f3eecbffadcbee4b71533e
zolandhole <zolandhole@gmail.com>
2019-02-21 14:48:43 +0700

    Alhamdulillah kemajuan

--- app/src/main/java/com/example/dialogislamgaruda/adapter/AdapterIklan.java
+++ app/src/main/java/com/example/dialogislamgaruda/adapter/AdapterIklan.java
@@ -1,27 +1,42 @@
 package com.example.dialogislamgaruda.adapter;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dialogislamgaruda.R;
 import com.example.dialogislamgaruda.model.ModelIklan;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class AdapterIklan extends RecyclerView.Adapter<AdapterIklan.MyViewHolder> {
     private LayoutInflater layoutInflater;
     private ArrayList<ModelIklan> modelIklanArrayList;
+    private Context context;
+    private Dialog iklanDialog;
+    private TextView textViewCaption;
+    private Button buttonShare;
+    private ImageView imageViewIklan, imageViewClose;
+//    private int test=0;
 
     public AdapterIklan(Context ctx, ArrayList<ModelIklan> modelIklanArrayList) {
         this.layoutInflater = LayoutInflater.from(ctx);
         this.modelIklanArrayList = modelIklanArrayList;
+        this.context = ctx;
     }
 
     @NonNull
@@ -32,8 +47,32 @@ public class AdapterIklan extends RecyclerView.Adapter<AdapterIklan.MyViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AdapterIklan.MyViewHolder holder, int i) {
-        Picasso.get().load(modelIklanArrayList.get(i).getImgURL()).into(holder.imageView);
+    public void onBindViewHolder(@NonNull AdapterIklan.MyViewHolder holder, @SuppressLint("RecyclerView") final int test) {
+        Picasso.get().load(modelIklanArrayList.get(test).getImgURL()).into(holder.imageView);
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iklanDialog = new Dialog(context);
+                iklanDialog.setContentView(R.layout.popup_iklan);
+                imageViewClose = iklanDialog.findViewById(R.id.close_iklan);
+                textViewCaption = iklanDialog.findViewById(R.id.tvCaption);
+                imageViewIklan = iklanDialog.findViewById(R.id.ivIklan);
+                imageViewClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        iklanDialog.dismiss();
+                    }
+                });
+                ModelIklan model = modelIklanArrayList.get(test);
+                Picasso.get().load(model.getImgURL()).into(imageViewIklan);
+                textViewCaption.setText(model.getCaption());
+//
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    Objects.requireNonNull(iklanDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                }
+                iklanDialog.show();
+            }
+        });
     }
 
     @Override
